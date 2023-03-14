18af9ce0d4cf92be861e2b810e2be5fb03d2ffcf
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2023-03-14 15:58:00 +0000

    Add snippet to get the token from the site

---
+++ snippet.js
@@ -0,0 +1,10 @@
+/*
+Login to https://coverflex.com/meal and paste this on the console
+Copy the output (value of `result`) and paste it on the token.json file
+*/
+const result = {
+	token: localStorage.getItem('cvrflx_flightdeck_token'),
+	refresh: localStorage.getItem('cvrflx_flightdeck_refresh_token'),
+};
+// eslint-disabled-next-line
+result
